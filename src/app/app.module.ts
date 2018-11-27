c25491e4a600bbb4fe364b5f48ec32475ec15ccc
Renan Pereira <renansoaresinfo@gmail.com>
2018-11-27 19:20:34 -0200

    Primeiro Commit / configuração do app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { MyApp } from './app.component';
-
-
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { FirebaseConfig } from '../config/firebase.config';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FirebaseConfig) //Inicializa o firebase conforme firebase.config.ts
   ],
   bootstrap: [IonicApp],
   entryComponents: [
